5a3f62ad650e6ac1da6572ada5153acb62956e64
Arthur Pointeau <arthur.pointeau@protonmail.com>
2026-08-10 19:35:32 +0200

    interactable ui except console, camera image loading. blue border on left eye

--- Assets/UVC4UnityAndroidPlugin/Scripts/UVCManager.cs
+++ Assets/UVC4UnityAndroidPlugin/Scripts/UVCManager.cs
@@ -1101,8 +1101,6 @@ namespace Serenegiant.UVC
 #if (!NDEBUG && DEBUG && ENABLE_LOG)
 			Console.WriteLine($"{TAG}Initialize:");
 #endif
-			if (AndroidUtils.CheckAndroidVersion(28))
-			{
 				yield return AndroidUtils.GrantCameraPermission((string permission, AndroidUtils.PermissionGrantResult result) =>
 				{
 #if (!NDEBUG && DEBUG && ENABLE_LOG)
@@ -1124,11 +1122,6 @@ namespace Serenegiant.UVC
 							break;
 					}
 				});
-			}
-			else
-			{
-				InitPlugin();
-			}
 
 			yield break;
 		}
