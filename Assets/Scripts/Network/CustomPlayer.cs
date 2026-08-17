fbc6e3e786986e1f98fce022e7384b59e3e6c4fd
Arthur Pointeau <arthur.pointeau@protonmail.com>
2026-08-17 19:35:51 +0200

    bit more log

--- Assets/Scripts/Network/CustomPlayer.cs
+++ Assets/Scripts/Network/CustomPlayer.cs
@@ -72,10 +72,10 @@ namespace Mirror.Examples.Pong
         
         private IEnumerator StartupDim() 
         {
-            Debug.Log("created pano, about to dim");
+            Debug.Log(gameObject.name + " created pano, about to dim");
             yield return new WaitForSeconds(2);
             _dimGameEvent.Raise(true);
-            Debug.Log("startup dim");
+            Debug.Log(gameObject.name + "startup dim");
         }
 
         private void Rotate()
